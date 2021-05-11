a9fddb17cddcbdbebc31f1d41618272317f705af
Chad Tarpey <chadtarpey@gmail.com>
2021-05-11 14:51:21 -0500

    created a UI services

--- angular-crash-todoList/src/app/app.module.ts
+++ angular-crash-todoList/src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
-import { AddTaskComponent } from './component/add-task/add-task.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { AddTaskComponent } from './component/add-task/add-task.component';
     AppRoutingModule,
     FontAwesomeModule,
     HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
